b7ef9812e4141024790bb7ac8731c2046868fbc1
Artem <64410376+Artiss90@users.noreply.github.com>
2021-04-30 21:48:07 +0300

    userSchema
    добавил модель User

--- model/schemas/contactSchema.js
+++ model/schemas/contactSchema.js
@@ -19,6 +19,10 @@ const schemaContact = new Schema(
       type: Boolean,
       default: false,
     },
+    owner: {
+      // type: SchemaTypes.ObjectId,
+      ref: "user",
+    },
   },
   {
     timestamps: true,
---
+++ model/schemas/userSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const schemaUser = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserSchema = model("user", schemaUser);
+
+module.exports = UserSchema;
